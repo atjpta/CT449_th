d3add7bdf5941dde402b539e850617ecdb7a2b28
atjpa <caoducan351@gmail.com>
2022-02-26 14:43:03 +0700

    cai dat express chay thong bao xin chao

---
+++ app/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    app: {
+        port: process.env.PORT || 8080,
+    },
+
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
+    }
+};
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const config = require('./app/config');
+
+
+// start sv
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Sever is running on port${PORT}.`);
+});
